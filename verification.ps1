a98fa0a19ca00ef19306874d824120b2fc9c8802
majerus noemie <majerusnoemi@gmail.com>
2025-12-22 21:21:33 +0100

    first commit

--- verification.ps1
+++ verification.ps1
@@ -1,3 +1,3 @@
-if (get-command -name "python") {Write-Host "python est installé"} else {Write-Host "python n'est pas installé"}
-node --version
+if (get-command -name python) {echo "python est installé"} else {echo "python n'est pas installé"}
+if (get-command -name node) {echo "Node est bien installé"} else {echo "Node n'est pas installé"}
 2+2
