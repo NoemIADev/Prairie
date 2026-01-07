aec6ba6a4da1a16f5ff2da522a6716e1b70300c1
NoemIADev <majerusnoemie@gmail.com>
2026-01-07 01:27:39 +0100

    finaly i did it

--- noemie-doc/octavia/dataframe.py
+++ noemie-doc/octavia/dataframe.py
@@ -1,17 +1,32 @@
-import numpy as np
 import pandas as pd
+import numpy as np
 import random
-A, B, C, D, V = int
-"1" = int
-"2" = int
-"3" = int
-ref = random.randrange(0,100,1)
-#cree une fonction qui remplit dataframe avec 1,2,3 et ref avec 1 ou 2 ou 3 qui est == à ref et les autre aleatoirement,puis A,B,C,D qui vont choisir une barre aleatoirement parmi 1,2,3 qui n'est pas egale a ref
-def auto() : #créer une fonction qui remplit un dataframe
-    ash_keys = ["1","2","3",ref,A,B,C,D,V]
-    df = pd.DataFrame(columns = ash_keys)
-    
+
+#créer une fonction qui remplit dataframe avec 1,2,3 et ref avec 1 ou 2 ou 3 qui est = à ref et les autre aleatoire mais tous different,puis A,B,C,D qui vont choisir une barre aleatoirement parmi 1,2,3 qui n'est pas egale a ref
+def auto() : #100 lignes de barres qui sont toujours differentes
+     valeurs_barres = [random.sample(range(0, 101), 3) for _ in range(100)]
+     # creer un frame avec des colonnes 1,2,3 pour valeurr "valeurs_barres"
+     df = pd.DataFrame(valeurs_barres,columns=["1","2","3"],)
+     # ajouter une colonne ref qui prend une valeur aleatoire parmi 1,2,3
+     df["ref"] = [random.choice(v) for v in valeurs_barres]
+     # determine les choix de colnonne des complice
+     cols = ["1","2","3"]
+     # on met dans finalChoices le choix final 
+     finalChoices = []
+     # pour chaqye ligne dans df
+     for l in range(len(df)):
+         # on prend la valeur de ref pas ref directement
+         ref_val = df.loc[l, "ref"]
+            # on determine les choix possibles parmis les colonne qui ne sont pas egale a ref
+         choix_possibles = [c for c in cols if df.loc[l, c] != ref_val]
+            # on ajoute un choix aleatoire parmi les choix possibles
+         finalChoices.append(random.choice(choix_possibles))
+     df["A"] = finalChoices
+     df["B"] = finalChoices
+     df["C"] = finalChoices
+     df["D"] = finalChoices
+     df["V"]= np.nan
 
 
-#barreRef = ref
-asche_df = pd.DataFrame#({ "1":None  ,"2": None, "3": None, ref: random.randrange(0, 100, 1), A: None, B: None, C: None, D: None, V :None})
+     return df
+print(auto())
